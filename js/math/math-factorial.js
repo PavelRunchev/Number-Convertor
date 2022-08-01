df8f3c306b7313b38ec6c80b49a641f2504acfa2
PavelRunchev <Setty_ii@abv.bg>
2022-08-01 13:25:16 +0300

    new design navigation header and footer

--- js/math/math-factorial.js
+++ js/math/math-factorial.js
@@ -2,6 +2,29 @@
 //factorial
 document.querySelector('.btn-factorial-calculate').addEventListener('click', factorial);
 document.querySelector('.code-factorial').innerHTML = `<pre>${codeFactorial}</pre`;
+let clipboard = document.querySelector('.clipboard-factorial');
+let coppiedClipBoard = document.querySelector('.coppidClipboard-factorial');
+let codeTagFactorial = document.querySelector('.code-factorial');
+
+if(clipboard == null || coppiedClipBoard == null || codeTagFactorial == null)
+    ErrorDomElement();
+
+clipboard.addEventListener('click', getCodeFactorial);
+coppiedClipBoard.addEventListener('click', returnClipboardFactorial);
+
+function getCodeFactorial(e) {
+    e.preventDefault();
+    coppiedClipBoard.style.display = 'block';
+    clipboard.style.display = 'none';
+    navigator.clipboard.writeText(codeTagFactorial.textContent);
+}
+
+function returnClipboardFactorial(e) {
+    e.preventDefault();
+    coppiedClipBoard.style.display = 'none';
+    clipboard.style.display = 'block';
+    navigator.clipboard.writeText('');
+}
 
 function factorial(e) {
     let input = document.querySelector('.input-factorial-number');
--- js/math/math-fibonacci.js
+++ js/math/math-fibonacci.js
@@ -1,6 +1,29 @@
 //fibonacci
 document.querySelector('.btn-fibonacci-calculate').addEventListener('click', fibonacci);
 document.querySelector('.code-fibonacci').innerHTML = `<pre>${codeFibonacci}</pre`;
+let clipboardFibonacci = document.querySelector('.clipboard-fibonacci');
+let coppiedClipBoardFibonacci = document.querySelector('.coppidClipboard-fibonacci');
+let codeTagFibonacci = document.querySelector('.code-fibonacci');
+
+if(clipboardFibonacci == null || coppiedClipBoardFibonacci == null || codeTagFibonacci == null)
+    ErrorDomElement();
+
+    clipboardFibonacci.addEventListener('click', getCodeFibonacci);
+coppiedClipBoardFibonacci.addEventListener('click', returnClipboardFibonacci);
+    
+function getCodeFibonacci(e) {
+    e.preventDefault();
+    coppiedClipBoardFibonacci.style.display = 'block';
+    clipboardFibonacci.style.display = 'none';
+    navigator.clipboard.writeText(codeTagFibonacci.textContent);
+}
+    
+function returnClipboardFibonacci(e) {
+    e.preventDefault();
+    coppiedClipBoardFibonacci.style.display = 'none';
+    clipboardFibonacci.style.display = 'block';
+    navigator.clipboard.writeText('');
+}
 
 function fibonacci(e) {
     let input = document.querySelector('.input-fibonacci-number');
